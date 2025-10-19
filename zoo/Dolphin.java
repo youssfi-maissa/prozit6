2bb3e7fa5378d8ebb28430d3a0800badd4f1b9b8
youssfi-maissa <Maissa.Yousfi@esprit.tn>
2025-10-19 16:45:53 +0100

    Create Dolphin.java

---
+++ zoo/Dolphin.java
@@ -0,0 +1,22 @@
+package zoo;
+
+public class Dolphin extends Aquatic {
+    protected float swimmingSpeed;
+
+    public Dolphin() {}
+
+    public Dolphin(String family, String name, int age, boolean isMammal, String habitat, float swimmingSpeed) {
+        super(family, name, age, isMammal, habitat);
+        this.swimmingSpeed = swimmingSpeed;
+    }
+
+    @Override
+    public void swim() {
+        System.out.println("This dolphin is swimming.");
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Vitesse=" + swimmingSpeed;
+    }
+}
